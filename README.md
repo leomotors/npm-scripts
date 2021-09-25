a7a93abd6742771566099c9e0e2f58e1f1efcda1
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-09-25 19:23:05 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# auto-increment-pkg-version
+
+A code that I used alot, so I decided to put in npm
---
+++ incrementVersion.js
@@ -0,0 +1,24 @@
+// * Increment Build Number by 1 and set to package.json
+
+/* eslint-disable */
+
+const fs = require("fs");
+const chalk = require("chalk");
+
+var package_data = JSON.parse(fs.readFileSync("./package.json").toString());
+const version = package_data.version;
+var vers = version.split(".");
+
+var appVersion = parseInt(vers[2]);
+var newversion = vers[0] + "." + vers[1] + "." + (appVersion + 1).toString();
+package_data.version = newversion;
+
+fs.writeFileSync("./package.json", JSON.stringify(package_data, null, 2));
+
+console.log(
+  chalk.green(
+    `[incrementVersion.js] Build Number Incremented : ${appVersion} -> ${
+      appVersion + 1
+    }\n`
+  )
+);
